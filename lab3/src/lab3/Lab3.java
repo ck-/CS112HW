7afc4a4c2b642ff479a6aa9d1040fb5111c92836
c_k <peacefullpeach@gmail.com>
2013-09-24 20:30:16 -0700

    lab3

---
+++ lab3/src/lab3/Lab3.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3;
+
+/**
+ *
+ * @author king
+ */
+import java.util.Scanner;
+   import java.util.Random;
+public class Lab3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner userInput = new Scanner( System.in );
+        
+         Random rand = new Random(); 
+         rand.nextInt(10); 
+         int pickedNumber = rand.nextInt(10) + 1;
+   System.out.println("Welcome to the random number game!"
+           + " Guess a number between one and ten.");
+       int guess = userInput.nextInt();
+       
+        if (guess>pickedNumber)
+            System.out.println("You guessed a "
+                    + "number greater than the picked number.");
+        
+                
+        
+        else if (guess<pickedNumber)
+            System.out.println("You guessed a number "
+                    + "less than the picked number.");
+        else 
+             
+            System.out.println("You got the number correct!");
+                  
+    }
+}
